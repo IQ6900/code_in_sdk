40c5c01af32828223c04b40f5174e8fd29692cf3
sumin <dwckey5356@gmail.com>
2025-07-18 17:39:44 +0900

    respose test

--- src/client.ts
+++ src/client.ts
@@ -275,7 +275,7 @@ export async function fetchChunksUntilComplete(txId: string) {
         i++;
         const url = `${iqHost}/get_transaction_chunks/${currentTx}`; //read transaction by 100 tx read
         const response = await fetch(url);
-
+        console.log(response)
         const data: any = await response.json();
 
         const chars = data.resultStr;
