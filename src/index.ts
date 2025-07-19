1a3da21072c5b24a6c0b34faa2111e135af5dae9
0sumTX <tomcruise007.of@gmail.com>
2025-07-18 23:47:22 -0300

    export getDBPDA

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {server_init,pda_check, user_init} from './transaction';
 import {readCode, fetchLargeFileAndDoCache, dataValidation, fetchDataSignatures, joinChat} from './reader';
 import {codeIn, codeInAfterErr, codeToUserWallet,codeToPDA} from './uploader';
-import {getServerPDA} from "./client";
+import {getServerPDA, getDBPDA} from "./client";
 import {getMyPublicKey} from "./utils";
 
 export default {
@@ -9,6 +9,7 @@ export default {
     user_init,
     server_init,
     pda_check,
+    getDBPDA,
     getServerPDA,
     readCode,
     fetchLargeFileAndDoCache,
